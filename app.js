da5c52ecddbb0c964007d9e0d935de187ec58da3
Remy Orans <remy.orans@gmail.com>
2020-12-07 12:00:44 -0800

    Implement hover

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // TODO
+let GroceryList = (props) => {
+  return (
+  <ul>
+    {props.list.map((item, key) => <GroceryListItem item={item} key={key}/>)}
+  </ul>
+  );
+};
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hover: false
+    };
+  }
+  onHover() {
+    this.setState({
+      hover: !this.state.hover
+    })
+  }
+
+  render () {
+    debugger;
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+    return (
+      <li style={style} onMouseEnter={this.onHover.bind(this)} onMouseLeave={this.onHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+
+
+
+ReactDOM.render(<GroceryList list = {['Milk', 'Eggs', 'Cheese']}/>, document.getElementById('app'));
+
+
+
